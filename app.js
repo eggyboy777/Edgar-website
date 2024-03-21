bf3f47bb5c6f8a32fc0a141781fb4652c49dbad8
eggyboy777 <144335552+eggyboy777@users.noreply.github.com>
2024-03-21 13:04:15 +0000

    cross ;)

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
+const menuLinks = document.querySelector('.navbar__menu');
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
